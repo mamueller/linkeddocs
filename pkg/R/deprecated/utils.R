5cea201a96b10751bb688cc1d633aba44c928860
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-06-09 13:24:57 +0200

    moved the unneded stuff

--- pkg/R/deprecated/utils.R
+++ pkg/R/deprecated/utils.R
@@ -73,26 +73,6 @@ findGeneric <- function(fname, envir){
   isUME(body(f))
 }
 
-### Copied from R-3.0.1, to support fixPackageFileNames.
-fixPackageFileNames <- function(list){
-  list <- as.character(list)
-  if (length(list) == 0L) 
-    return(list)
-  list0 <- gsub("[[:cntrl:]\"*/:<>?\\|]", "_", list)
-  #list0 <- gsub("#", "_", list0) #mm this was necessary to get rid of th "#" occuring in method signature strings as separator  
-  wrong <- grep("^(con|prn|aux|clock\\$|nul|lpt[1-3]|com[1-4])(\\..*|)$", 
-                list0)
-  if (length(wrong)) 
-    list0[wrong] <- paste0("zz", list0[wrong])
-  ok <- grepl("^[[:alnum:]]", list0)
-  if (any(!ok)) 
-    list0[!ok] <- paste0("z", list0[!ok])
-  list1 <- tolower(list0)
-  list2 <- make.unique(list1, sep = "_")
-  changed <- (list2 != list1)
-  list0[changed] <- list2[changed]
-  list0
-}
 
 ### Copied from R-3.2.2, and changed to write the PackageRd File 
 mm_promptPackage= function (package, filename,lib.loc = NULL, description) 
