96e3079434d174b4c34bc16a3b4d7126726c2929
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-06-20 12:54:34 +0200

    added the seealso and details section to the documentation.

--- pkg/R/2_1_DocOject.R
+++ pkg/R/2_1_DocOject.R
@@ -66,3 +66,36 @@ setMethod(
     return(var_names)
   }
 )
+#-------------------------------------------------------------------------
+setMethod(
+  f="write_Rd_file",
+  signature=signature(obj="docObject",fn="character"),
+  def=function(
+      obj,
+      fn
+    ){
+    d=obj@l
+    #the list d is nested e.g. for argumetns
+    #we now flatten it so that it only has a 
+    # character vector for each section
+    flat<-list()
+    flat[["name"]]  <- obj@name
+    flat[["alias"]] <- obj@name
+	  flat[["usage"]] <- Rd_usage_lines(obj)
+
+    # for generic functions it is possible 
+    # that no arguments have been documented
+    # in setGeneric
+    # Of cause the methods will define arguments.
+    args<-Rd_argument_lines(obj)
+    if (!is.null(args)){flat[["arguments"]]<-args} 
+    # add the parts from d that could be extracted 
+    target_secs<-c("title","description","details","references","note","seealso","value")
+    for (sec in target_secs){
+      if (is.element(sec,names(d))){
+        flat[[sec]]<-d[[sec]]
+      }
+    }
+    writeFlattenedListToRd(flat,fn)
+  }
+)
