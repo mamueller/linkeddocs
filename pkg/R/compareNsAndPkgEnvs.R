b2a2f6157e46a23cddeb07a9bce452907ead2b12
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-10-02 18:14:54 +0200

    found out how to add the srcrefs to expressions and use them

--- pkg/R/compareNsAndPkgEnvs.R
+++ pkg/R/compareNsAndPkgEnvs.R
@@ -3,39 +3,47 @@
 # The purpose of this function is to find
 # a way to load the package to document into
 # two separate environments
-compareNsAndPkgEnvs<- function(pkgDir){
+compareNsAndPkgEnvs<- function(pkgDir,taskFunc){
   require(devtools)
-  #source_env <- devtools:::create_ns_env(pkgDir)
+  pkgName<-as.character(read.dcf(file=file.path(pkgDir,'DESCRIPTION'),fields='Package'))
+  # create but do not load the namespace
   source_env <- devtools::: makeNamespace(pkgName, 1.1)
-  pe(quote(loadedNamespaces()))
+  #pe(quote(loadedNamespaces()))
   #pp('source_env')
+
   pkgR<-normalizePath(file.path(pkgDir,'R'))
   codeFiles <- list.files(pkgR,full.names=TRUE)
   #exprs <- c()
   results <- list()
   j=1
   for (fn in codeFiles){
+    print(fn)
     lines <- readLines(fn)
     sf <- srcfile(fn)
     exprs <- parse(text=lines,srcfile=sf,keep.source=TRUE)
-    #print(sprintf('getParseData=%s',getParseData(exprs)))
+    srcreflist <- attr(exprs,'srcref')
+    pd <- getParseData(exprs)
     n <- length(exprs)
     for (i in seq_len(n)){
+      print(j)
       expr <- exprs[[i]]
       res <- eval(expr,source_env)
+      pe(quote(getSrcref(expr)))
       results[[j]] <- list()
       results[[j]][['res']] <- res
-      results[[j]][['srcRef']] <- getParseData(expr)
+      results[[j]][['srcRef']] <-srcreflist[[i]]
       j=j+1
     }
   }
-  pe(quote(getClasses(source_env)))
-  #unload(pkgDir)
-  unloadNamespace('pkgName')
-  pe(quote(getClasses(source_env)))
-  pkgName<-as.character(read.dcf(file=file.path(pkgDir,'DESCRIPTION'),fields='Package'))
+ # pe(quote(getClasses(source_env)))
+ # #unload(pkgDir)
+ # unloadNamespace('pkgName')
+ # pe(quote(getClasses(source_env)))
   require(pkgName,character.only=TRUE)
   fqPkgName <- sprintf("package:%s",pkgName)
   pkgEnv <- as.environment(fqPkgName) 
-  pe(quote(getClasses(pkgEnv)))
+ # pe(quote(getClasses(pkgEnv)))
+  taskFunc(pkgEnv,source_env,results)
+  
+  return(results)
 }
--- tests/minitest.R
+++ tests/minitest.R
@@ -5,8 +5,39 @@ require('linkeddocs')
 pkgName <- 'PrivateAndPublic'
 pkgDir <- sprintf('IoTestResults_tmp/PackageTests_3.test.%s/pkg',pkgName)
 devtools::install(pkgDir)
-compareNsAndPkgEnvs(pkgDir)
 
+
+taskFunc <- function(pkgEnv,nsEnv,results){
+	exClNs <- getClasses(pkgEnv)
+	allClNs <- getClasses(nsEnv)
+	pe(quote(exClNs))
+	pe(quote(allClNs))
+	for (clname in exClNs){
+		# we want to find the source reference for every exported class
+		clRep <- getClass(clname,pkgEnv)
+		# find out the name of the class and find the code that created it
+	  # we are looking for an expression that created a classGeneratorFuntion (this is what setClass returns) 
+	  # but this classGeneratorFunction might not be bound to an environment # It is only bound if it was assigned by a statement like
+	  # M <- setClass("M"...
+	  ff <- function(entry){
+						retval <- FALSE
+						res <- entry[['res']]
+						if (inherits(res,'classGeneratorFunction')){
+							retval <- res@className==clname
+						}
+						retval
+		}
+	  bv <- unlist(lapply(results,ff))
+		srcRefs <- results[bv]
+    if (length(srcRefs)>1){
+						stop(fprintf('Found more than one definition for Class %s in %s',clname,srcRefs))
+		}
+		pe(quote(srcRefs[[1]][['srcRef']]))
+	}
+	
+}
+results <- compareNsAndPkgEnvs(pkgDir,taskFunc)
+print(results)
 #linkeddocs::package.skeleton.dx_3(pkgDir)
 #dnse <-  devtools::load_all(pkgDir)
 ##env=dnse$env
