0dfa6c45867e697569259e8ffd9c29047c10a716
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-10-16 18:48:26 +0200

    small alteration

--- tests/inst.R
+++ tests/inst.R
@@ -2,7 +2,7 @@
 if (!is.element('devtools',installed.packages())){
 	install.packages('devtools',repos='https://cran.uni-muenster.de')
 }
-#require(devtools)
+require(devtools)
 succ <- devtools::install('~/linkeddocs/pkg/')
 require(getopt)
 
