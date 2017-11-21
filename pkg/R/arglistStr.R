478f512acba1432c150cf858ee37bfbec1c691b6
Markus Müller <markus.mueller.1.g@googlemail.com>
2017-11-21 19:55:06 +0100

    had treat NULL as a default value for a formal parameter in a special way

--- pkg/R/arglistStr.R
+++ pkg/R/arglistStr.R
@@ -6,16 +6,16 @@ arglistStr=function(functionObject){
   fa  <-formals(functionObject)
   argnames=names(fa)
   fun <- function(argname){
-      if (fa[[argname]]!=""){
-        return(
-          .widthCutter( 
-            sprintf("%s=%s",argname,as.expression(fa[[argname]])),
-            89
-          )
-        )
+      if(!is.null(fa[[argname]])){
+        if (fa[[argname]]!=""){
+              s <- as.expression(fa[[argname]])
+        }else{
+          s <- ' '
+        }
       }else{
-        return(argname)
+        s <- 'NULL'
       }
+      return( .widthCutter( sprintf("%s=%s",argname,s) , 89))
   }
   arglistWithDefaults<-unlist(lapply(argnames,fun))
   arglistStr<-paste0(arglistWithDefaults,collapse=",\n")
