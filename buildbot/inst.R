a5b844f4d32382ba49c96e0cb3b6465a7157f416
Markus Müller <markus.mueller.1.g@googlemail.com>
2018-07-06 22:09:57 +0200

    the SoilR installation is only successful if deSolve has been installed before. Ohterwise a shared lib for gfortran is missing which is weired. So I added deSolve to the install script

--- buildbot/inst.R
+++ buildbot/inst.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
-fromCran <- c( 'devtools')
+fromCran <- c('devtools','deSolve')
 lapply(fromCran,install.packages,repos='https://cran.uni-muenster.de')
 
 fromGitHub <- c( 'R6Unit')
