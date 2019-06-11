bd06f31170075e135a665b2901aa5f2bb8acbf26
Markus Müller <markus.mueller.1.g@googlemail.com>
2019-06-11 17:47:53 +0200

    reenabled the install of R6Unit from github instead of a local dir (which had been overridden for testing)

--- travis/inst.R
+++ travis/inst.R
@@ -3,10 +3,10 @@
 fromCran <- c('stringr','devtools','remotes','pkgload','deSolve')
 lapply(fromCran,install.packages,repos='https://cran.uni-muenster.de')
 #
-#fromGitHub <- c( 'R6Unit')
-#require(remotes)
-#lapply(fromGitHub,function(name){remotes::install_github(sprintf("mamueller/%s/pkg",name))})
+fromGitHub <- c( 'R6Unit')
+require(remotes)
+lapply(fromGitHub,function(name){remotes::install_github(sprintf("mamueller/%s/pkg",name))})
 
 # install the package itself from source  
-devtools::install(file.path('/home/mm/R6Unit/pkg'))
+#devtools::install(file.path('/home/mm/R6Unit/pkg'))
 devtools::install(file.path('pkg'))
